2382348b97d7f8d17208e50c9f342fb363c5063a
RahmatEffendi <rahmat480@gmail.com>
2018-01-16 21:08:21 +0700

    perulangan for

---
+++ for.rb
@@ -0,0 +1,27 @@
+=begin
+==========================================================================
+= Pembahasan tentang for
+= For merupakan salah satu perulangan yang digunakan oleh ruby
+= Bentuk umum dari for adalah,
+=
+= for variable in ekspresi do
+=	# aksi
+= end
+= 
+= ekspresi pada for bisa menggunakan range, array maupun hash
+==========================================================================	
+=end
+
+#Penggunaan for untuk ekspresi bertipe jangkauan
+#for i in 1..8 do
+#	puts "#{i} adalah jangkauan ke #{i}"
+	# keluaran dari puts diatas akan menhasilkan nilai dari 1 - 8, dengan perulangan yang terus dilakukan sampai nilai tersebut
+	# bernilai false atau nilai yang berada pada range telah mencapai batasannya yaitu 8.
+	# untuk mempermudah, kita dapa menambahkan method sleep(2) dengan lamanya sleep sebesar 2 detik, bisa diubah sesuai keinginan
+#	sleep(2)
+	#if i == 8 then
+	#	puts "Nilai Berakhir"
+	#end
+#end
+
+
---
+++ latihan/latihan1.rb
@@ -0,0 +1,10 @@
+=begin
+================================================================================================
+= Latihan Penggunaan Perulangan, Pemilihan Pada Ruby
+= Created by : Rahmat Effendi
+= Jakarta, 16-1-2018
+=
+================================================================================================
+=end
+
+#Perulangan Pilihan Menu
