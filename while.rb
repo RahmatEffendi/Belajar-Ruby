2c408c07702928d66b8c62308dd74ed93673ce95
RahmatEffendi <rahmat480@gmail.com>
2018-01-17 10:00:17 +0700

    Penambahan Source untuk while

---
+++ while.rb
@@ -0,0 +1,44 @@
+######################################################################
+#Perulangan dengan menggunakan while
+#Bentuk umum dari penggunaan while adalah
+#
+# while kondisi do
+# 	#aksi
+# end
+#
+######################################################################
+
+#simple code to use while
+#i = 1
+#while i <= 3 do
+#	puts "Welcome to Ruby Programming"
+#	i += 1
+#end
+
+#Using sleep() method in while
+#i = 1
+#while i <= 10 do
+#	puts"#{i} adalah nilai ke #{i}"
+#	print"\n"
+#	sleep(2)
+#	i += 1
+#end
+
+#Perhitungan nilai total
+#print "Masukkan nilai : "
+#n = gets.to_i #ubah inputan menjadi integer untuk float bisa menggunakan to_f
+
+#total = 0
+#i = 1
+
+#while i <= n do
+#	print i.to_s
+#	if i < n then
+#		print " + "
+#	else
+#		print " = "
+#	end
+#	total += i
+#	i += 1
+#end
+#puts total.to_s
