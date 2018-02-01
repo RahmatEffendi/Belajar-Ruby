f4abb7d881ba9ea28f514aabcfb3e56ef25284df
RahmatEffendi <rahmat480@gmail.com>
2018-02-01 10:57:27 +0700

    Penggunaan Case Dalam Menentukan Hari

--- case.rb
+++ case.rb
@@ -20,19 +20,38 @@
 #
 =end
 
-print "Masukkan Nilai : "
-nilai = STDIN.getc()
+print "Masukkan Angka Dari 1 - 10: "
+noHari = gets.to_i
 
-case nilai
-when 'a'..'z', 'A'..'Z'
-	puts "Tidak Bole Memasukkan Alfabet"
-else
-	nilai = nilai.to_i
-	if nilai % 2 == 0 
-		puts "Nilai #{nilai.to_s} adalah Genap"
-	else
-		puts "Nilai #{nilai.to_s} adalah Ganjil"
-	end
+if noHari > 7 || noHari < 1
+	puts "Nilai Tidak Boleh Lebih besar dari 7 dan kecil dari 1"
 end
+
+case noHari
+	when 1 then hari = "Senin"
+	when 2 then hari = "Selasa"
+	when 3 then hari = "Rabu"
+	when 4 then hari = "Kamis"
+	when 5 then hari = "Jumat"
+	when 6 then hari = "Sabtu"
+	when 7 then hari = "Minggu"
+end
+
+puts "Nomer #{noHari} adalah Hari #{hari}"
+
+#print "Masukkan Nilai : "
+#nilai = STDIN.getc()
+#
+#case nilai
+#when 'a'..'z', 'A'..'Z'
+#	puts "Tidak Bole Memasukkan Alfabet"
+#else
+#	nilai = nilai.to_i
+#	if nilai % 2 == 0 
+#		puts "Nilai #{nilai.to_s} adalah Genap"
+#	else
+#		puts "Nilai #{nilai.to_s} adalah Ganjil"
+#	end
+#end
 
 
