a45fff589411eabc21b67708c11c21a8dc946da3
RahmatEffendi <rahmat480@gmail.com>
2018-04-02 15:21:11 +0700

    update source method.rb

--- method.rb
+++ method.rb
@@ -29,9 +29,39 @@
 #   method dengan nilai balik adalah method yang akan mengembalikan nilai ketika method tersebut 
 #dieksekusi contoh gets()
 
-print "Masukkan Nilai : "
-nilai = gets.to_i
-puts nilai
+#print "Masukkan Nilai : "
+#nilai = gets.to_i
+#puts nilai
 
 #Jika nilai yang ingin dimasukkan hanya khusus integer saja atau string saja ataupun float saja
 #bisa disisipkan method lain, misalnya ingin integer saja, makan disisipkan method to_i()
+
+#Method tanpa nilai balik secara umum ditulis
+
+    #def NamaMethod(parameterMethod)
+        #isi method
+    #end
+
+#Method dengan nilai balik secara umum ditulis
+
+    #def NamaMethod(parameterMethod)
+        #isi method
+    #    return nilai
+    #end
+
+#Contoh Penggunaan Method Sederhana
+#def helloworld()
+#    return print "Hello World"
+#end
+
+#cara pemanggilan method, cukup hanya memanggil nama dari method tersebut.
+#helloworld()
+
+#Bagaimana jika pemanggilan medthod nya memiliki parameter, gampang saja, tinggal memasukkan
+#parameter dari method tersebut. Misalnya :
+def helloworld_2(nama)
+    return print "Hello " + nama
+end
+
+#panggil methodnya nama parameter yang harus memiliki value
+helloworld_2("rahmat")
