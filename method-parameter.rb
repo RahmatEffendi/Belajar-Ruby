65f3018b03fe7c7c03a938128f73acb853497d4e
RahmatEffendi <rahmat480@gmail.com>
2018-04-16 09:10:20 +0700

    update source

--- method-parameter.rb
+++ method-parameter.rb
@@ -43,3 +43,53 @@
 #cetak('ruby', ', ')
 #barisBaru = "\n"
 #cetak('java', barisBaru)
+
+
+#def riwayat(nama:, alamat:, ttl:, usia:)
+#    puts "Nama : #{nama}"
+#    puts "Alamat : #{alamat}"
+#    puts "Tempat/Tgl Lahir : #{ttl}"
+#    puts "Usia : #{usia}"
+#end
+
+#print "Nama : "
+#nama = gets.to_s
+#print "Alamat : "
+#alamat = gets.to_s
+#print "Tempat/Tgl Lahir : "
+#ttl = gets.to_s
+#print "Usia : "
+#usia = gets.to_s
+
+#riwayat(nama:nama, alamat:alamat, ttl:ttl, usia:usia)
+
+=begin
+    pada contoh diatas parameter yang digunakan pada method lebih dari satu atau dua parameter, pada pembahasana
+    sebelumnya telah dijelaskan bahwa method bisa memiliki lebih dari satu parameter dan bisa memiliki banyak
+    parameter. Pada method diatas merupakan parameter yang menggunakan keyword argument. Pemanggilan dari pada
+    parameter - parameter di atas bisa dipanggil tanpa harus berurutan dari nama sampai usia, bisa dimulai dari
+    usia, alamat, nama, ttl dan sebagainya.
+    
+    Agar parameter dapat dipanggil, disetiap parameter pada method di atas ditampahkan titik dua : , setelah
+    tanda titik dua kemudian diikuti oleh parameter-parameter yang akan dipanggil, dapat dilihat pada contoh di atas
+    ketika user telah memasukkan semua input yang diminta kemudian method dieksekusi pada parameter method dapat dilihat
+    setiap parameter menggunakan titik dua dan nama dari parameternya, misalnya nama:nama. Parameter kedua dari nama
+    bisa menggunakan kata sesuai keiinginan, misalnya nama:nm , Namun karakter nama diawal harus sesuai dengan parameter
+    yang telah didefinisikan pada method.
+=end
+
+#contoh lain dengan penggunaan word berbeda dari parameter
+def data(nama_awal:, nama_akhir:, nama_alias:)
+    puts "Nama Awal : #{nama_awal}"
+    puts "Nama Akhir : #{nama_akhir}"
+    puts "Alias : #{nama_alias}"
+end
+
+print "Nama Awal : "
+first = gets.to_s
+print "Nama Akhir : "
+second = gets.to_s
+print "Nama Alias : "
+alay = gets.to_s
+
+data(nama_awal:first, nama_akhir:second, nama_alias:alay)
